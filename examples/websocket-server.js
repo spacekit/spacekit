585557b980d879a23e9157260ce5cf370a6f5bc7
Marcus Cavanaugh <m@mcav.com>
2016-03-01 15:15:34 -0800

    wss server

--- examples/websocket-server.js
+++ examples/websocket-server.js
@@ -1,7 +1,19 @@
 'use strict';
 const WebSocketServer = require('ws').Server;
+const https = require('https');
+const fs = require('fs');
 
-const wss = new WebSocketServer({ port: process.env.PORT });
+const options = {
+  key: fs.readFileSync(process.env.KEY),
+  cert: fs.readFileSync(process.env.CERT)
+};
+
+const httpsServer = https.createServer(options);
+httpsServer.listen(443);
+
+const wss = new WebSocketServer({
+  server: httpsServer
+});
 
 wss.on('connection', (ws) => {
   console.log('GOT CONNECTION');
