81d04896694e9f2879b89a40a9f27af1a457eb9c
Marcus Cavanaugh <m@mcav.com>
2016-05-04 13:24:16 -0700

    Add an end-to-end integration test.

--- tests/test-config.js
+++ tests/test-config.js
@@ -1,12 +1,12 @@
 'use strict';
-const t = require('tap');
+const tap = require('tap');
 const parseConfig = require('../lib/config');
 
-t.throws(() => {
+tap.throws(() => {
   parseConfig('-r', null, true);
 }, { message: /Missing required arguments: u, a/ });
 
 let config = parseConfig('-r relay -u user -a key -p 8000 -p 9000:example.com:9001', null, true);
-t.match(config.portMap.get(443), { hostname: 'localhost', destinationPort: 8000 });
-t.match(config.portMap.get(9000), { hostname: 'example.com', destinationPort: 9001 });
-t.equal(config.portMap.get(10000), undefined);
+tap.match(config.portMap.get(443), { hostname: 'localhost', destinationPort: 8000 });
+tap.match(config.portMap.get(9000), { hostname: 'example.com', destinationPort: 9001 });
+tap.equal(config.portMap.get(10000), undefined);
