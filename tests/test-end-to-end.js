974bb92fd44df7f07218fb9651f0b7db2f83352a
Marcus Cavanaugh <m@mcav.com>
2016-05-31 16:52:55 -0700

    Allow self-signed certificates for testing.

--- tests/test-end-to-end.js
+++ tests/test-end-to-end.js
@@ -1,9 +1,14 @@
 'use strict';
+
+// Allow us to use self-signed certificates for testing.
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
+
 const tap = require('tap');
 const SpaceKitService = require('spacekit-service');
 const Relay = require('../lib/index');
 const PortMap = require('../lib/port-map');
 const Http = require('http');
+const Https = require('https');
 
 tap.test('end to end', (tap) => {
   let httpServer = Http.createServer((req, res) => {
@@ -13,31 +18,34 @@ tap.test('end to end', (tap) => {
   httpServer.on('listening', () => {
     let httpPort = httpServer.address().port;
     let service = new SpaceKitService({
-      host: '127.0.0.1.nip.io:8000',
+      host: '127.0.0.1.nip.io',
       api: 'api',
-      tcpPort: 8000
+      httpsPort: 9443,
+      httpPort: 9080
     });
 
     let map = new PortMap();
-    map.addFromString('8000:' + httpPort);
+    map.addFromString('9443:' + httpPort);
     let relay = new Relay({
       noTls: true,
       relay: 'relay',
       username: 'user',
-      host: '127.0.0.1.nip.io',
+      host: '127.0.0.1.nip.io:9443',
       apiKey: '',
       service: 'api',
       portMap: map
     });
 
     relay.on('connected', () => {
-      Http.get('http://relay.user.127.0.0.1.nip.io:8000/', (res) => {
+      Https.get('https://relay.user.127.0.0.1.nip.io:9443/', (res) => {
         tap.equal(res.statusCode, 200);
         relay.close();
         httpServer.close();
         service.close();
         tap.end();
       });
+    }).on('error', (err) => {
+      tap.threw(err);
     });
   });
 
